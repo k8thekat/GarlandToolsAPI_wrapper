f0ffdd57b8401c2a74c4b2a1d313039b822b8950
Lukas Weber <me@sakul6499.de>
2022-08-12 00:45:09 +0200

    Added tests

---
+++ garlandtools/test_author.py
@@ -0,0 +1,4 @@
+from garlandtools import __author__
+
+def test_version():
+    assert __author__ == "Lukas Weber"
---
+++ garlandtools/test_credits.py
@@ -0,0 +1,4 @@
+from garlandtools import __credits__
+
+def test_version():
+    assert __credits__ == "GarlandTools"

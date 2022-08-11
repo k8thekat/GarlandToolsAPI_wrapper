3423860a718f9a71de0db5e4b644bcd4941e306e
Lukas Weber <me@sakul6499.de>
2022-08-12 01:19:16 +0200

    Imports should be at the top of the file

--- garlandtools/__init__.py
+++ garlandtools/__init__.py
@@ -2,13 +2,14 @@
 GarlandTools Python Module
 """
 
+import requests_cache
+
 __version__ = "0.1.0"
 __author__ = 'Lukas Weber'
 __credits__ = 'GarlandTools'
 
 GARLAND_TOOLS_ENDPOINT = 'https://www.garlandtools.org'
 
-import requests_cache
 
 session = requests_cache.CachedSession('garlandtools_cache', backend='sqlite', expire_after=3600)
 result = session.get(GARLAND_TOOLS_ENDPOINT)

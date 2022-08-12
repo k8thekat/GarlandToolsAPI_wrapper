159c155162947c7e7f2ffbbeab0bafac8107c58d
Lukas Weber <me@sakul6499.de>
2022-08-12 18:44:11 +0200

    Fixed PyLint complaint

--- garlandtools/endpoint_achievement.py
+++ garlandtools/endpoint_achievement.py
@@ -4,15 +4,18 @@ Wrapper for GarlandTools Item Endpoint
 
 from .globals import GARLAND_TOOLS_ENDPOINT, GARLAND_TOOLS_LANGUAGE, SESSION
 
-GARLAND_TOOLS_ACHIEVEMENT_ENDPOINT = f'{GARLAND_TOOLS_ENDPOINT}db/doc/achievement/{GARLAND_TOOLS_LANGUAGE}/2/'
-GARLAND_TOOLS_ACHIEVEMENTS_ENDPOINT = f'{GARLAND_TOOLS_ENDPOINT}db/doc/browse/{GARLAND_TOOLS_LANGUAGE}/2/achievement.json'
+GARLAND_TOOLS_ACHIEVEMENT_ENDPOINT = f'{GARLAND_TOOLS_ENDPOINT}db/doc/' \
+    + f'achievement/{GARLAND_TOOLS_LANGUAGE}/2/'
+GARLAND_TOOLS_ACHIEVEMENTS_ENDPOINT = f'{GARLAND_TOOLS_ENDPOINT}db/doc/' \
+    + f'browse/{GARLAND_TOOLS_LANGUAGE}/2/achievement.json'
 
 
 def achievement(achievement_id: int):
     """
     Returns an achievement by id
     """
-    result = SESSION.get(f'{GARLAND_TOOLS_ACHIEVEMENT_ENDPOINT}{achievement_id}.json')
+    result = SESSION.get(
+        f'{GARLAND_TOOLS_ACHIEVEMENT_ENDPOINT}{achievement_id}.json')
     return result
 
 

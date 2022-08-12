a5e87c364efb6da5638a6eaa30a6d0dd7408b7ae
Lukas Weber <me@sakul6499.de>
2022-08-12 22:34:34 +0200

    PyLint compliance

--- garlandtools/test_endpoint_fate.py
+++ garlandtools/test_endpoint_fate.py
@@ -10,7 +10,8 @@ def test_fate_is_ok():
     Tests if an fate request succeeds
     """
     response = fate(
-        1631)  # Don't even ask me why it's starting at that number. This is one of the first Lv1 FATEs.
+        1631)   # Don't even ask me why it's starting at that number.
+    # This is one of the first Lv1 FATEs.
     assert response.ok
 
 
@@ -19,7 +20,8 @@ def test_fate_is_json():
     Tests if an fate request returns JSON
     """
     response = fate(
-        1631)  # Don't even ask me why it's starting at that number. This is one of the first Lv1 FATEs.
+        1631)   # Don't even ask me why it's starting at that number.
+    # This is one of the first Lv1 FATEs.
     assert response.ok
 
     response_json = response.json()

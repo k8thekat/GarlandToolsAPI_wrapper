6b911770f228aa9e082672c630294252a6346d2f
Lukas Weber <me@sakul6499.de>
2022-08-12 17:22:00 +0200

    Removed global definitions from __init__

--- garlandtools/__init__.py
+++ garlandtools/__init__.py
@@ -9,9 +9,3 @@ from .endpoint_item import *
 __version__ = "0.1.0"
 __author__ = 'Lukas Weber'
 __credits__ = 'GarlandTools'
-
-GARLAND_TOOLS_ENDPOINT = 'https://www.garlandtools.org'
-
-
-session = requests_cache.CachedSession('garlandtools_cache', backend='sqlite', expire_after=3600)
-result = session.get(GARLAND_TOOLS_ENDPOINT)

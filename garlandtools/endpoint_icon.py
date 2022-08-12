3888b77d3888cf0fcc79d4fb189e9bb4eb46ad6e
Lukas Weber <me@sakul6499.de>
2022-08-12 23:07:25 +0200

    PyLint changes

--- garlandtools/endpoint_icon.py
+++ garlandtools/endpoint_icon.py
@@ -7,9 +7,10 @@ from .globals import GARLAND_TOOLS_ENDPOINT, SESSION
 GARLAND_TOOLS_ICON_ENDPOINT = f'{GARLAND_TOOLS_ENDPOINT}files/icons/'
 
 
-def icon(type: str, id: int):
+def icon(icon_type: str, icon_id: int):
     """
-    Returns a specific icon by type and id
+    Returns a specific icon by icon_type and icon_id
     """
-    result = SESSION.get(f'{GARLAND_TOOLS_ICON_ENDPOINT}{type}/{id}.png')
+    result = SESSION.get(
+        f'{GARLAND_TOOLS_ICON_ENDPOINT}{icon_type}/{icon_id}.png')
     return result

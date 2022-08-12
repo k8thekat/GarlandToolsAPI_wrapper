c3f175520837c2e4b95873b7cff84418dfa348ec
Lukas Weber <me@sakul6499.de>
2022-08-12 22:45:01 +0200

    Test fixes

--- garlandtools/test_endpoint_data.py
+++ garlandtools/test_endpoint_data.py
@@ -18,5 +18,7 @@ def test_data_is_json():
     Tests if an data request returns JSON
     """
     response = data()
+    assert response.ok
+
     response_json = response.json()
     assert isinstance(response_json, dict)

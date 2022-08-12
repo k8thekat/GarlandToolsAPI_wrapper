3727512ff105d3d396392724513dd0e392bfffb1
Lukas Weber <me@sakul6499.de>
2022-08-12 22:32:11 +0200

    Added fate endpoint

--- garlandtools/__init__.py
+++ garlandtools/__init__.py
@@ -8,6 +8,7 @@ from .endpoint_item import *
 from .endpoint_achievement import *
 from .endpoint_data import *
 from .endpoint_endgame_gear import *
+from .endpoint_fate import *
 
 
 __version__ = "0.1.0"

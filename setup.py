ded426d671bd0122c201707df4d574b01c809a8a
Lukas Weber <me@sakul6499.de>
2022-08-15 17:29:24 +0200

    Updated version to 2.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='garlandtools',
-    version='1.0.1',
+    version='2.0.0',
     description='Python PIP module for GarlandTools',
     url='https://github.com/Sakul6499/GarlandTools-PIP',
     author='Lukas Weber',
